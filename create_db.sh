4933e29d7d1b898addeab092cff4d03d42775b8d
Antony Holmes <antony@antonybholmes.com>
2024-12-30 23:05:10 -0500

    fix: Bug fixes and updates.

--- create_db.sh
+++ create_db.sh
@@ -1,6 +1,7 @@
 for f in data/modules/genes/*.sql
 do
     out=`echo data/modules/genes/${f} | sed -r 's/.sql/.db/'`
+    echo ${f} ${out}
     rm ${out}
     cat data/modules/genes/${f} | sqlite3 ${out}
     sqlite3 ${out} 'PRAGMA journal_mode=WAL;'
